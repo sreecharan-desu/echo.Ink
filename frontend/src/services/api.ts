f98169b9f68fb815825d17db084d36fef27ee618
sreecharan-desu <sreecharan309@gmail.com>
2024-12-13 23:56:35 +0530

    url change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://echoink-api.vercel.app/';
+const BASE_URL = 'https://echoink-api.vercel.app';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
